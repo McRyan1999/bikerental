77d8a03475846d13d79ff34ffff67e26777b7e3f
John McRyan <john_mcryan_aunja_bi21@iluv.ums.edu.my>
2025-01-14 14:30:19 +0800

    Update my-booking.php

--- my-booking.php
+++ my-booking.php
@@ -2,183 +2,159 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
-  {
-header('location:index.php');
+
+// Redirect to login page if session is not active
+if (strlen($_SESSION['login']) == 0) {
+    header('location:index.php');
+    exit();
 }
-else{
-?><!DOCTYPE HTML>
+
+// Function to fetch user data
+function fetchUserData($dbh, $useremail) {
+    $sql = "SELECT * FROM tblusers WHERE EmailId = :useremail";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':useremail', $useremail, PDO::PARAM_STR);
+    $query->execute();
+    return $query->fetch(PDO::FETCH_OBJ);
+}
+
+// Function to fetch bookings
+function fetchUserBookings($dbh, $useremail) {
+    $sql = "SELECT 
+                tblvehicles.Vimage1 AS Vimage1, 
+                tblvehicles.VehiclesTitle AS VehiclesTitle, 
+                tblvehicles.id AS vid, 
+                tblbrands.BrandName AS BrandName, 
+                tblbooking.FromDate AS FromDate, 
+                tblbooking.ToDate AS ToDate, 
+                tblbooking.message AS message, 
+                tblbooking.Status AS Status 
+            FROM tblbooking 
+            JOIN tblvehicles ON tblbooking.VehicleId = tblvehicles.id 
+            JOIN tblbrands ON tblbrands.id = tblvehicles.VehiclesBrand 
+            WHERE tblbooking.userEmail = :useremail";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':useremail', $useremail, PDO::PARAM_STR);
+    $query->execute();
+    return $query->fetchAll(PDO::FETCH_OBJ);
+}
+
+// Fetch user details and bookings
+$useremail = $_SESSION['login'];
+$userData = fetchUserData($dbh, $useremail);
+$userBookings = fetchUserBookings($dbh, $useremail);
+?>
+<!DOCTYPE HTML>
 <html lang="en">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<meta name="keywords" content="">
-<meta name="description" content="">
-<title>BikeForYou - Responsive Bike Dealer HTML5 Template</title>
-<!--Bootstrap -->
-<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
-<!--Custome Style -->
-<link rel="stylesheet" href="assets/css/styles.css" type="text/css">
-<!--OWL Carousel slider-->
-<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
-<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
-<!--slick-slider -->
-<link href="assets/css/slick.css" rel="stylesheet">
-<!--bootstrap-slider -->
-<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
-<!--FontAwesome Font Style -->
-<link href="assets/css/font-awesome.min.css" rel="stylesheet">
-
-<!-- SWITCHER -->
-		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
-
-<!-- Fav and touch icons -->
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
-<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
-<link rel="shortcut icon" href="assets/images/favicon-icon/24x24.png">
-<!-- Google-Font-->
-<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BikeForYou - My Bookings</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
 </head>
+
 <body>
+    <!-- Start Switcher -->
+    <?php include('includes/colorswitcher.php'); ?>
+    <!-- /Switcher -->
 
-<!-- Start Switcher -->
-<?php include('includes/colorswitcher.php');?>
-<!-- /Switcher -->
+    <!--Header-->
+    <?php include('includes/header.php'); ?>
+    <!-- /Header -->
 
-<!--Header-->
-<?php include('includes/header.php');?>
-<!--Page Header-->
-<!-- /Header -->
-
-<!--Page Header-->
-<section class="page-header profile_page">
-  <div class="container">
-    <div class="page-header_wrap">
-      <div class="page-heading">
-        <h1>My Booking</h1>
-      </div>
-      <ul class="coustom-breadcrumb">
-        <li><a href="#">Home</a></li>
-        <li>My Booking</li>
-      </ul>
-    </div>
-  </div>
-  <!-- Dark Overlay-->
-  <div class="dark-overlay"></div>
-</section>
-<!-- /Page Header-->
-
-<?php
-$useremail=$_SESSION['login'];
-$sql = "SELECT * from tblusers where EmailId=:useremail";
-$query = $dbh -> prepare($sql);
-$query -> bindParam(':useremail',$useremail, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{ ?>
-<section class="user_profile inner_pages">
-  <div class="container">
-    <div class="user_profile_info gray-bg padding_4x4_40">
-      <div class="upload_user_logo"> <img src="assets/images/dealer-logo.jpg" alt="image">
-      </div>
-
-      <div class="dealer_info">
-        <h5><?php echo htmlentities($result->FullName);?></h5>
-        <p><?php echo htmlentities($result->Address);?><br>
-          <?php echo htmlentities($result->City);?>&nbsp;<?php echo htmlentities($result->Country); }}?></p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-3 col-sm-3">
-       <?php include('includes/sidebar.php');?>
-
-      <div class="col-md-6 col-sm-8">
-        <div class="profile_wrap">
-          <h5 class="uppercase underline">My Bookings </h5>
-          <div class="my_vehicles_list">
-            <ul class="vehicle_listing">
-<?php
-$useremail=$_SESSION['login'];
- $sql = "SELECT tblvehicles.Vimage1 as Vimage1,tblvehicles.VehiclesTitle,tblvehicles.id as vid,tblbrands.BrandName,tblbooking.FromDate,tblbooking.ToDate,tblbooking.message,tblbooking.Status  from tblbooking join tblvehicles on tblbooking.VehicleId=tblvehicles.id join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand where tblbooking.userEmail=:useremail";
-$query = $dbh -> prepare($sql);
-$query-> bindParam(':useremail', $useremail, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{  ?>
-
-<li>
-                <div class="vehicle_img"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->vid);?>""><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" alt="image"></a> </div>
-                <div class="vehicle_title">
-                  <h6><a href="vehical-details.php?vhid=<?php echo htmlentities($result->vid);?>""> <?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a></h6>
-                  <p><b>From Date:</b> <?php echo htmlentities($result->FromDate);?><br /> <b>To Date:</b> <?php echo htmlentities($result->ToDate);?></p>
+    <!-- Page Header -->
+    <section class="page-header profile_page">
+        <div class="container">
+            <div class="page-header_wrap">
+                <div class="page-heading">
+                    <h1>My Booking</h1>
                 </div>
-                <?php if($result->Status==1)
-                { ?>
-                <div class="vehicle_status"> <a href="#" class="btn outline btn-xs active-btn">Confirmed</a>
-                           <div class="clearfix"></div>
+                <ul class="coustom-breadcrumb">
+                    <li><a href="index.php">Home</a></li>
+                    <li>My Booking</li>
+                </ul>
+            </div>
         </div>
+        <div class="dark-overlay"></div>
+    </section>
+    <!-- /Page Header -->
 
-              <?php } else if($result->Status==2) { ?>
- <div class="vehicle_status"> <a href="#" class="btn outline btn-xs">Cancelled</a>
-            <div class="clearfix"></div>
+    <section class="user_profile inner_pages">
+        <div class="container">
+            <div class="user_profile_info gray-bg padding_4x4_40">
+                <div class="upload_user_logo">
+                    <img src="assets/images/dealer-logo.jpg" alt="User Image">
+                </div>
+                <div class="dealer_info">
+                    <h5><?php echo htmlentities($userData->FullName); ?></h5>
+                    <p>
+                        <?php echo htmlentities($userData->Address); ?><br>
+                        <?php echo htmlentities($userData->City); ?>, <?php echo htmlentities($userData->Country); ?>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <?php include('includes/sidebar.php'); ?>
+                </div>
+                <div class="col-md-9">
+                    <div class="profile_wrap">
+                        <h5 class="uppercase underline">My Bookings</h5>
+                        <div class="my_vehicles_list">
+                            <ul class="vehicle_listing">
+                                <?php if ($userBookings): ?>
+                                    <?php foreach ($userBookings as $booking): ?>
+                                        <li>
+                                            <div class="vehicle_img">
+                                                <a href="vehical-details.php?vhid=<?php echo htmlentities($booking->vid); ?>">
+                                                    <img src="admin/img/vehicleimages/<?php echo htmlentities($booking->Vimage1); ?>" alt="Vehicle Image">
+                                                </a>
+                                            </div>
+                                            <div class="vehicle_title">
+                                                <h6>
+                                                    <a href="vehical-details.php?vhid=<?php echo htmlentities($booking->vid); ?>">
+                                                        <?php echo htmlentities($booking->BrandName); ?>, <?php echo htmlentities($booking->VehiclesTitle); ?>
+                                                    </a>
+                                                </h6>
+                                                <p>
+                                                    <b>From Date:</b> <?php echo htmlentities($booking->FromDate); ?><br>
+                                                    <b>To Date:</b> <?php echo htmlentities($booking->ToDate); ?>
+                                                </p>
+                                            </div>
+                                            <div class="vehicle_status">
+                                                <?php if ($booking->Status == 1): ?>
+                                                    <a href="#" class="btn outline btn-xs active-btn">Confirmed</a>
+                                                <?php elseif ($booking->Status == 2): ?>
+                                                    <a href="#" class="btn outline btn-xs">Cancelled</a>
+                                                <?php else: ?>
+                                                    <a href="#" class="btn outline btn-xs">Not Confirmed</a>
+                                                <?php endif; ?>
+                                                <div class="clearfix"></div>
+                                            </div>
+                                            <p><b>Message:</b> <?php echo htmlentities($booking->message); ?></p>
+                                        </li>
+                                    <?php endforeach; ?>
+                                <?php else: ?>
+                                    <p>No bookings found.</p>
+                                <?php endif; ?>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </section>
 
+    <?php include('includes/footer.php'); ?>
 
-
-                <?php } else { ?>
- <div class="vehicle_status"> <a href="#" class="btn outline btn-xs">Not Confirm yet</a>
-            <div class="clearfix"></div>
-        </div>
-                <?php } ?>
-       <div style="float: left"><p><b>Message:</b> <?php echo htmlentities($result->message);?> </p></div>
-              </li>
-              <?php }} ?>
-
-
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!--/my-vehicles-->
-<?php include('includes/footer.php');?>
-
-<!-- Scripts -->
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/interface.js"></script>
-<!--Switcher-->
-<script src="assets/switcher/js/switcher.js"></script>
-<!--bootstrap-slider-JS-->
-<script src="assets/js/bootstrap-slider.min.js"></script>
-<!--Slider-JS-->
-<script src="assets/js/slick.min.js"></script>
-<script src="assets/js/owl.carousel.min.js"></script>
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/interface.js"></script>
 </body>
+
 </html>
-<?php } ?>
