97fc65683eb00b854d082863c17a09e3a3ed98bf
John McRyan <john_mcryan_aunja_bi21@iluv.ums.edu.my>
2025-01-17 17:44:36 +0800

    Update bike-listing.php

--- bike-listing.php
+++ bike-listing.php
@@ -7,54 +7,25 @@ error_reporting(0);
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<meta name="keywords" content="">
-<meta name="description" content="">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Bike Rental Portal | Bike Listing</title>
-<!--Bootstrap -->
-<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
-<!--Custome Style -->
-<link rel="stylesheet" href="assets/css/styles.css" type="text/css">
-<!--OWL Carousel slider-->
-<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
-<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
-<!--slick-slider -->
+
+<!-- Stylesheets -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/styles.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css">
 <link href="assets/css/slick.css" rel="stylesheet">
-<!--bootstrap-slider -->
 <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
-<!--FontAwesome Font Style -->
 <link href="assets/css/font-awesome.min.css" rel="stylesheet">
-
-<!-- SWITCHER -->
-		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
-
-<!-- Fav and touch icons -->
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
-<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
-<link rel="shortcut icon" href="assets/images/favicon-icon/24x24.png">
-<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 </head>
+
 <body>
+<!-- Header -->
+<?php include('includes/header.php'); ?>
 
-<!-- Start Switcher -->
-<?php include('includes/colorswitcher.php');?>
-<!-- /Switcher -->
-
-<!--Header-->
-<?php include('includes/header.php');?>
-<!-- /Header -->
-
-<!--Page Header-->
+<!-- Page Header -->
 <section class="page-header listing_page">
   <div class="container">
     <div class="page-header_wrap">
@@ -62,169 +33,157 @@ error_reporting(0);
         <h1>Bike Listing</h1>
       </div>
       <ul class="coustom-breadcrumb">
-        <li><a href="#">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li>Bike Listing</li>
       </ul>
     </div>
   </div>
-  <!-- Dark Overlay-->
-  <div class="dark-overlay"></div>
 </section>
-<!-- /Page Header-->
 
-<!--Listing-->
+<!-- Bike Listings Section -->
 <section class="listing-page">
   <div class="container">
     <div class="row">
+      <!-- Main Content -->
       <div class="col-md-9 col-md-push-3">
         <div class="result-sorting-wrapper">
           <div class="sorting-count">
-<?php
-//Query for Listing count
-$sql = "SELECT id from tblvehicles";
-$query = $dbh -> prepare($sql);
-$query->bindParam(':vhid',$vhid, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=$query->rowCount();
-?>
-<p><span><?php echo htmlentities($cnt);?> Listings</span></p>
-</div>
-</div>
-
-<?php $sql = "SELECT tblvehicles.*,tblbrands.BrandName,tblbrands.id as bid  from tblvehicles join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{  ?>
-        <div class="product-listing-m gray-bg">
-          <div class="product-listing-img"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" class="img-responsive" alt="Image" /> </a>
-          </div>
-          <div class="product-listing-content">
-            <h5><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a></h5>
-            <p class="list-price">$<?php echo htmlentities($result->PricePerDay);?> Per Day</p>
-            <ul>
-              <li><i class="fa fa-user" aria-hidden="true"></i><?php echo htmlentities($result->SeatingCapacity);?> seats</li>
-              <li><i class="fa fa-calendar" aria-hidden="true"></i><?php echo htmlentities($result->ModelYear);?> model</li>
-              <li><i class="fa fa-motorcycle" aria-hidden="true"></i><?php echo htmlentities($result->FuelType);?></li>
-            </ul>
-            <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>" class="btn">View Details <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a>
+            <?php
+            // Get Total Listings Count
+            $sql = "SELECT COUNT(id) AS total FROM tblvehicles";
+            $query = $dbh->prepare($sql);
+            $query->execute();
+            $countResult = $query->fetch(PDO::FETCH_OBJ);
+            ?>
+            <p><span><?php echo htmlentities($countResult->total); ?> Listings</span></p>
           </div>
         </div>
-      <?php }} ?>
-         </div>
 
-      <!--Side-Bar-->
+        <!-- Fetch and Display Bike Listings -->
+        <?php
+        $sql = "SELECT tblvehicles.*, tblbrands.BrandName 
+                FROM tblvehicles 
+                JOIN tblbrands ON tblbrands.id = tblvehicles.VehiclesBrand";
+        $query = $dbh->prepare($sql);
+        $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_OBJ);
+
+        if ($query->rowCount() > 0) {
+          foreach ($results as $result) { ?>
+            <div class="product-listing-m gray-bg">
+              <div class="product-listing-img">
+                <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>">
+                  <img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1); ?>" class="img-responsive" alt="Bike Image">
+                </a>
+              </div>
+              <div class="product-listing-content">
+                <h5>
+                  <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>">
+                    <?php echo htmlentities($result->BrandName); ?>, <?php echo htmlentities($result->VehiclesTitle); ?>
+                  </a>
+                </h5>
+                <p class="list-price">$<?php echo htmlentities($result->PricePerDay); ?> Per Day</p>
+                <ul>
+                  <li><i class="fa fa-user"></i> <?php echo htmlentities($result->SeatingCapacity); ?> seats</li>
+                  <li><i class="fa fa-calendar"></i> <?php echo htmlentities($result->ModelYear); ?> model</li>
+                  <li><i class="fa fa-motorcycle"></i> <?php echo htmlentities($result->FuelType); ?></li>
+                </ul>
+                <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id); ?>" class="btn">View Details</a>
+              </div>
+            </div>
+          <?php }
+        } else {
+          echo "<p class='text-center'>No bikes available at the moment.</p>";
+        }
+        ?>
+      </div>
+
+      <!-- Sidebar -->
       <aside class="col-md-3 col-md-pull-9">
         <div class="sidebar_widget">
           <div class="widget_heading">
-            <h5><i class="fa fa-filter" aria-hidden="true"></i> Find Your  Bike </h5>
+            <h5><i class="fa fa-filter"></i> Find Your Bike</h5>
           </div>
           <div class="sidebar_filter">
             <form action="search-carresult.php" method="post">
               <div class="form-group select">
                 <select class="form-control" name="brand">
                   <option>Select Brand</option>
+                  <?php
+                  $sql = "SELECT * FROM tblbrands";
+                  $query = $dbh->prepare($sql);
+                  $query->execute();
+                  $brands = $query->fetchAll(PDO::FETCH_OBJ);
 
-                  <?php $sql = "SELECT * from  tblbrands ";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{       ?>
-<option value="<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?></option>
-<?php }} ?>
-
+                  foreach ($brands as $brand) {
+                    echo "<option value='" . htmlentities($brand->id) . "'>" . htmlentities($brand->BrandName) . "</option>";
+                  }
+                  ?>
                 </select>
               </div>
               <div class="form-group select">
                 <select class="form-control" name="fueltype">
                   <option>Select Fuel Type</option>
-<option value="Petrol">Petrol</option>
-<option value="Diesel">Diesel</option>
-<option value="CNG">CNG</option>
+                  <option value="Petrol">Petrol</option>
+                  <option value="Diesel">Diesel</option>
+                  <option value="CNG">CNG</option>
                 </select>
               </div>
-
-              <div class="form-group">
-                <button type="submit" class="btn btn-block"><i class="fa fa-search" aria-hidden="true"></i> Search Bike</button>
-              </div>
+              <button type="submit" class="btn btn-block"><i class="fa fa-search"></i> Search Bike</button>
             </form>
           </div>
         </div>
 
+        <!-- Recently Listed Bikes -->
         <div class="sidebar_widget">
           <div class="widget_heading">
-            <h5><i class="fa fa-motorcycle" aria-hidden="true"></i> Recently Listed Bikes</h5>
+            <h5><i class="fa fa-motorcycle"></i> Recently Listed Bikes</h5>
           </div>
           <div class="recent_addedcars">
             <ul>
-<?php $sql = "SELECT tblvehicles.*,tblbrands.BrandName,tblbrands.id as bid  from tblvehicles join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand order by id desc limit 4";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{  ?>
+              <?php
+              $sql = "SELECT tblvehicles.*, tblbrands.BrandName 
+                      FROM tblvehicles 
+                      JOIN tblbrands ON tblbrands.id = tblvehicles.VehiclesBrand 
+                      ORDER BY id DESC 
+                      LIMIT 4";
+              $query = $dbh->prepare($sql);
+              $query->execute();
+              $recentBikes = $query->fetchAll(PDO::FETCH_OBJ);
 
-              <li class="gray-bg">
-                <div class="recent_post_img"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" alt="image"></a> </div>
-                <div class="recent_post_title"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a>
-                  <p class="widget_price">$<?php echo htmlentities($result->PricePerDay);?> Per Day</p>
-                </div>
-              </li>
-              <?php }} ?>
-
+              if ($query->rowCount() > 0) {
+                foreach ($recentBikes as $bike) { ?>
+                  <li class="gray-bg">
+                    <div class="recent_post_img">
+                      <a href="vehical-details.php?vhid=<?php echo htmlentities($bike->id); ?>">
+                        <img src="admin/img/vehicleimages/<?php echo htmlentities($bike->Vimage1); ?>" alt="Bike Image">
+                      </a>
+                    </div>
+                    <div class="recent_post_title">
+                      <a href="vehical-details.php?vhid=<?php echo htmlentities($bike->id); ?>">
+                        <?php echo htmlentities($bike->BrandName); ?>, <?php echo htmlentities($bike->VehiclesTitle); ?>
+                      </a>
+                      <p class="widget_price">$<?php echo htmlentities($bike->PricePerDay); ?> Per Day</p>
+                    </div>
+                  </li>
+                <?php }
+              } else {
+                echo "<p class='text-center'>No recently listed bikes available.</p>";
+              }
+              ?>
             </ul>
           </div>
         </div>
       </aside>
-      <!--/Side-Bar-->
     </div>
   </div>
 </section>
-<!-- /Listing-->
 
-<!--Footer -->
-<?php include('includes/footer.php');?>
-<!-- /Footer-->
+<!-- Footer -->
+<?php include('includes/footer.php'); ?>
 
-<!--Back to top-->
-<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
-<!--/Back to top-->
-
-<!--Login-Form -->
-<?php include('includes/login.php');?>
-<!--/Login-Form -->
-
-<!--Register-Form -->
-<?php include('includes/registration.php');?>
-
-<!--/Register-Form -->
-
-<!--Forgot-password-Form -->
-<?php include('includes/forgotpassword.php');?>
-
-<!-- Scripts -->
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
 <script src="assets/js/interface.js"></script>
-<!--Switcher-->
-<script src="assets/switcher/js/switcher.js"></script>
-<!--bootstrap-slider-JS-->
-<script src="assets/js/bootstrap-slider.min.js"></script>
-<!--Slider-JS-->
-<script src="assets/js/slick.min.js"></script>
-<script src="assets/js/owl.carousel.min.js"></script>
-
 </body>
 </html>
